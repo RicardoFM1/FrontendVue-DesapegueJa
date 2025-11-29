7c1e7bb0fc79831a677291966e3bd2a1c4fd1262
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-11-28 21:29:12 -0300

    perfil com o header

--- src/pages/perfil/perfil.vue
+++ src/pages/perfil/perfil.vue
@@ -1,5 +1,119 @@
 <template>
-  <v-container style="display: flex" class="pa-6" v-if="tokenExiste">
+  <v-layout>
+
+ 
+  <v-container style="display: flex" class="pa-6 mt-10" v-if="tokenExiste">
+    <v-app-bar>
+       
+        <v-spacer></v-spacer>
+         <v-btn
+    variant="flat"
+    prepend-icon="mdi-home"
+    color="#1976D2"
+    @click="toHome"
+    :disabled="carregando"
+  >
+    Home
+  </v-btn>
+        <v-badge
+  :content="carrinho.length"
+  color="primary"
+  class="ml-4 mr-4"
+>
+  <v-btn
+    variant="flat"
+    prepend-icon="mdi-cart"
+    color="#3fa34f"
+    @click="toCarrinho"
+    :disabled="carregando"
+  >
+    Carrinho
+  </v-btn>
+</v-badge>
+
+          <v-menu v-model="menu" offset-y location="bottom end">
+            <template #activator="{ props }">
+              <v-tooltip top>
+                <template #activator="{ props: tooltip }">
+                  <v-btn
+                    v-bind="{ ...props, ...tooltip }"
+                    :disabled="carregando"
+                    variant="text"
+                  >
+                    <v-icon>mdi-dots-vertical</v-icon>
+                  </v-btn>
+                </template>
+                <span>Mais opções</span>
+              </v-tooltip>
+            </template>
+
+            <v-card class="pa-4" width="300">
+              <v-row justify="center">
+                <v-avatar
+                  size="70"
+                  class="d-flex align-center justify-center"
+                  :color="avatarUsuario.tipo === 'imagem' ? '' : 'indigo'"
+                >
+                  <template v-if="avatarUsuario.tipo === 'imagem'">
+                    <v-img :src="avatarUsuario.src" cover />
+                  </template>
+
+                  <template v-else>
+                    <span class="text-white text-h6 font-weight-bold">
+                      {{ avatarUsuario.texto }}
+                    </span>
+                  </template>
+                </v-avatar>
+              </v-row>
+              <v-row justify="center">
+                <v-tooltip top>
+                  <template #activator="{ props }">
+                    <div v-bind="props" class="pa-1 nomeUsuario ellipses">
+                      {{ usuario?.Nome }}
+                    </div>
+                  </template>
+                  <span>{{ usuario?.Nome }}</span>
+                </v-tooltip>
+              </v-row>
+
+              <v-row justify="center">
+                <v-tooltip top>
+                  <template #activator="{ props }">
+                    <div v-bind="props" class="pa-1 emailUsuario ellipses">
+                      {{ usuario?.email }}
+                    </div>
+                  </template>
+                  <span>{{ usuario?.email }}</span>
+                </v-tooltip>
+              </v-row>
+
+              <v-divider class="my-3"></v-divider>
+
+              <v-btn
+                block
+                color="#eaece7"
+                variant="flat"
+                class="mb-4"
+                prepend-icon="mdi-account"
+                @click="toPerfil"
+                :disabled="carregando"
+              >
+                PERFIL
+              </v-btn>
+              <v-btn
+                block
+                color="#cc0000"
+                variant="flat"
+                class="mb-4"
+                prepend-icon="mdi-logout"
+                @click="buttonSairClicado = !buttonSairClicado"
+                :disabled="carregando"
+                >
+                SAIR
+              </v-btn>
+            </v-card>
+          </v-menu>
+        </v-app-bar>
     <v-container class="containerAllPerfil">
       <div v-if="loadingInit" class="text-center pa-10">
         <v-progress-circular indeterminate color="primary" size="64" />
@@ -305,6 +419,47 @@
         </v-btn>
       </v-row>
       <v-dialog
+          max-width="500"
+          v-model="buttonSairClicado"
+          v-if="buttonSairClicado"
+        >
+          <v-card class="pa-4" elevation="8" rounded="xl">
+            <v-card-title class="text-center font-weight-bold text-h4">
+              <v-icon color="error" size="32" class="mr-2"
+                >mdi-alert-circle-outline</v-icon
+              >
+              Confirmar saída
+            </v-card-title>
+
+            <v-card-text class="text-center text-h5 text-medium-emphasis">
+              Tem certeza de que deseja sair da sua conta?
+            </v-card-text>
+
+            <v-card-actions class="justify-center mt-2">
+              <v-btn
+                color="grey"
+                variant="outlined"
+                rounded="xl"
+                @click="buttonSairClicado = false"
+                width="120"
+              >
+                Cancelar
+              </v-btn>
+
+              <v-btn
+                color="error"
+                variant="flat"
+                rounded="xl"
+                width="120"
+                :loading="loadingLogout"
+                @click="FazerLogout"
+              >
+                Sair
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      <v-dialog
         max-width="500"
         v-model="confirmacaoSair"
         v-if="confirmacaoSair"
@@ -347,6 +502,7 @@
       </v-dialog>
     </v-container>
   </v-container>
+   </v-layout>
 </template>
 
 <script setup>
@@ -408,6 +564,10 @@ const readOnlyComCEP = computed(() => {
   const numeros = endereco.value.Cep?.replace(/\D/g, "") || "";
   return numeros.length === 8;
 });
+const carregando = ref(false)
+const menu = ref(false);
+const buttonSairClicado = ref(false)
+const loadingLogout = ref(false)
 
 const rulesEmail = [
   (v) =>
@@ -509,6 +669,52 @@ const formatData = (value) => {
 };
 
 
+const carrinho = ref([])
+
+async function getCarrinho() {
+  if(token.value){
+  try {
+
+    const res = await connection.get(`/desapega/carrinho/${retrieve?.value.id}`);
+    
+    if (res.status == 200 || res.status == 201) {
+      carrinho.value = res.data;
+    } else {
+      toast.error("Estamos com dificuldade de listar seu carrinho...");
+    }
+  } catch (err) {
+    toast.error(err.response?.data?.message || "Erro ao listar seu carrinho");
+  }
+}
+}
+const getIniciais = (nome) => {
+  if (!nome) return "?";
+  const partes = nome.trim().split(" ");
+  if (partes.length === 1) return partes[0].charAt(0).toUpperCase();
+  return (
+    partes[0].charAt(0) + partes[partes.length - 1].charAt(0)
+  ).toUpperCase();
+};
+
+const avatarUsuario = computed(() => {
+  const nome = usuario.value?.Nome || "Usuário";
+  const foto = usuario.value?.foto_Perfil;
+
+  if (foto && foto !== "null" && foto !== "Sem imagem" && foto.trim() !== "") {
+    if (foto.startsWith("data:image")) {
+      return { tipo: "imagem", src: foto };
+    }
+
+    if (foto.startsWith("/9j/"))
+      return { tipo: "imagem", src: `data:image/jpeg;base64,${foto}` };
+    if (foto.startsWith("iVBORw0KGgo"))
+      return { tipo: "imagem", src: `data:image/png;base64,${foto}` };
+
+    return { tipo: "iniciais", texto: getIniciais(nome) };
+  }
+
+  return { tipo: "iniciais", texto: getIniciais(nome) };
+});
 
 
 function abrirExplorador() {
@@ -573,10 +779,45 @@ onMounted(async () => {
 
 
 
-  try {
+try {
     const res = await connection.get("/desapega/usuarios/retrieve", {
       headers: { Authorization: `Bearer ${token.value}` },
     });
+
+    if (res && res.status === 200 && res.data) {
+      retrieve.value = res.data;
+      usuario.value = res.data;
+      console.log("FOTO PERFIL REAL:", res.data.foto_Perfil);
+      console.log("Tamanho da string:", res.data.foto_Perfil?.length);
+      console.log("RETORNO API:", res.data);
+
+      if (
+        retrieve.foto_Perfil &&
+        retrieve.foto_Perfil !== "null" &&
+        retrieve.foto_Perfil !== ""
+      ) {
+        if (retrieve.foto_Perfil.startsWith("data:image")) {
+          retrieve.foto_Perfil = retrieve.foto_Perfil;
+        } else if (retrieve.foto_Perfil.startsWith("/9j/")) {
+          retrieve.foto_Perfil = `data:image/jpeg;base64,${retrieve.foto_Perfil}`;
+        } else if (retrieve.foto_Perfil.startsWith("iVBORw0KGgo")) {
+          retrieve.foto_Perfil = `data:image/png;base64,${retrieve.foto_Perfil}`;
+        } else if (
+          retrieve.foto_Perfil.startsWith("R0lGODlh") ||
+          retrieve.foto_Perfil.startsWith("R0lGODdh")
+        ) {
+          retrieve.foto_Perfil = `data:image/gif;base64,${retrieve.foto_Perfil}`;
+        } else {
+          retrieve.foto_Perfil = `data:image/png;base64,${retrieve.foto_Perfil}`;
+        }
+      } else {
+        retrieve.foto_Perfil = null;
+      }
+    } else {
+      toast.error("Erro ao buscar o usuário");
+      console.error("Resposta inesperada:", res);
+    }
+
 const telRaw = String(res.data.telefone || "").replace(/\D/g, "");
 
 let ddi = "55";
@@ -673,6 +914,7 @@ const ExcluirConta = async () => {
   } finally {
     loadingExclusao.value = false;
   }
+  await getCarrinho();
 };
 
 
@@ -755,4 +997,29 @@ const salvarAlteracoesEndereco = async () => {
     loadingEndereco.value = false;
   }
 };
+function toHome() {
+  router.push("/")
+}
+function toPerfil() {
+  if (tokenExiste.value == false) {
+    modalAlertShow.value = !modalAlertShow.value;
+    return;
+  }
+  router.push("/perfil");
+}
+function toCarrinho() {
+  if (tokenExiste.value == false) {
+    modalAlertShow.value = !modalAlertShow.value;
+    return;
+  }
+  router.push("/carrinho");
+}
+function FazerLogout() {
+  loadingLogout.value = true;
+  setTimeout(() => {
+    localStorage.removeItem("token");
+    router.push("/login");
+    loadingLogout.value = false;
+  }, 2000);
+}
 </script>
